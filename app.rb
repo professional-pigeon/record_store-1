ebf9679a9d65e5d9a6766ecaa78ea27ecc8b52da
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-19 11:39:41 -0700

    add remaining files for CRUD functionality
    Co-authored-by: mireie <mireie@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -19,12 +19,21 @@ get('/albums') do
   erb(:albums)
 end
 
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
+end
+
 get('/albums/new') do
   erb(:new_album)
 end
 
 get('/albums/:id') do
-  "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
 
 post('/albums') do
@@ -32,15 +41,22 @@ post('/albums') do
 end
 
 get('/albums/:id/edit') do
-  "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
 end
 
 patch('/albums/:id') do
-  "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
 end
 
 delete('/albums/:id') do
-  "This route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
 end
 
 get('/custom_route') do
