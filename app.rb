b40cca25049447e34a264eb65916aa6ee1e5ccee
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-19 10:46:28 -0700

    add initial files for albums and new albums
    Co-authored-by: mireie <mireie@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,17 +1,26 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/album')
+require('pry')
 also_reload('lib/**/*.rb')
 
+get('/test') do
+  @something = "this is a variable"
+  erb(:whatever)
+end
+
 get('/') do
-  "This will be our home page. '/' is always the root route in a Sinatra application."
+  @albums = Album.all
+  erb(:albums)
 end
 
 get('/albums') do
-  "This route will show a list of all albums."
+  @albums = Album.all
+  erb(:albums)
 end
 
 get('/albums/new') do
-  "This will take us to a page with a form for adding a new album."
+  erb(:new_album)
 end
 
 get('/albums/:id') do
